38c3b922076e4486a8ccde01411343cc5948113b
AdityaAdi1 <adityavermaindore@gmail.com>
2023-08-19 17:11:54 +0530

    final Project

---
+++ src/data.js
@@ -0,0 +1,38 @@
+//This file is ussed for adding data that has to be connected to database
+
+const data = [
+  {
+    "name": "Deti",
+    "subscribedChannel": "Adibetter"
+  },
+  {
+    "name": "Aditya",
+    "subscribedChannel": "FreeCodeCamp"
+  },
+  {
+    "name": "shikha",
+    "subscribedChannel": "Inspired"
+  },
+  {
+    "name": "rohit",
+    "subscribedChannel": "sony-music"
+  },
+  {
+    "name": "Aakash",
+    "subscribedChannel": "FAF"
+  },
+  {
+    "name": "Bhavani",
+    "subscribedChannel": "F1:Drive to Survive"
+  },
+  {
+    "name": "varun",
+    "subscribedChannel": "MrBeast"
+  },
+  {
+    "name": "Vansh",
+    "subscribedChannel": "Sony SAB"
+  }
+];
+
+module.exports = data;
